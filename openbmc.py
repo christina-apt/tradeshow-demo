4637d01aa8ff2d38c45046debccdb12cff7aba35
Christina Peng <christina.portwell@gmail.com>
2025-04-02 16:05:48 -0700

    change to webbrowser

--- openbmc.py
+++ openbmc.py
@@ -1,15 +1,9 @@
-import tkinter as tk
-import tkinterweb
+import webbrowser
 
-class OpenBMC(tk.Frame):
-    def __init__(self, master, background="background1.jpg"):
-        super().__init__(master)  # Initialize the parent tk.Frame class
-        self.master.geometry("1024x768")
-        self.master.title("Embedded Web Browser")
-        self.load_browser()
+class OpenBMC:
+    def __init__(self):
+        self.open_browser()
 
-    def load_browser(self):
-        # Create an HtmlFrame to load the web page
-        frame = tkinterweb.HtmlFrame(self.master)
-        frame.load_website("https://google.com")  # Load the desired URL
-        frame.pack(fill="both", expand=True)  # Expand the frame to fill the window
+    def open_browser(self):
+        url = "http://root:Apt44200@200.200.200.15"
+        webbrowser.open_new(url)
